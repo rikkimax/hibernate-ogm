0f0b596ec0b0301d354e016deca2dbaf8e86faa3
rikki molecatert <pbdadmin@gmail.com>
2011-11-12 13:48:09 +1300

    OGM Implemented basic operations for a MongoDB database.

--- hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/impl/MongoDBDialect.java
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/impl/MongoDBDialect.java
@@ -20,12 +20,18 @@
  */
 package org.hibernate.ogm.dialect.mongodb.impl;
 
+import com.mongodb.*;
 import org.hibernate.LockMode;
 import org.hibernate.dialect.lock.LockingStrategy;
 import org.hibernate.id.IntegralDataTypeHolder;
+import org.hibernate.ogm.datastore.impl.EmptyTupleSnapshot;
+import org.hibernate.ogm.datastore.impl.MapBasedTupleSnapshot;
+import org.hibernate.ogm.datastore.mongodb.impl.MongoDBDatastoreProvider;
 import org.hibernate.ogm.datastore.spi.Association;
 import org.hibernate.ogm.datastore.spi.Tuple;
+import org.hibernate.ogm.datastore.spi.TupleSnapshot;
 import org.hibernate.ogm.dialect.GridDialect;
+import org.hibernate.ogm.dialect.infinispan.InfinispanTupleSnapshot;
 import org.hibernate.ogm.grid.AssociationKey;
 import org.hibernate.ogm.grid.EntityKey;
 import org.hibernate.ogm.grid.RowKey;
@@ -37,29 +43,91 @@ import org.hibernate.persister.entity.Lockable;
  * @author Rikki Molecatert <pbdadmin@gmail.com>
  */
 public class MongoDBDialect implements GridDialect {
+    /**
+     * The provider that gives access to a MongoDB database.
+     */
+    private final MongoDBDatastoreProvider provider;
+
+    /**
+     * Constructor for the dialect.
+     * @param provider The MongoDB provider.
+     */
+    MongoDBDialect(MongoDBDatastoreProvider provider) {
+        this.provider = provider;
+    }
+
     @Override
     public LockingStrategy getLockingStrategy(Lockable lockable, LockMode lockMode) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        // How does this relate to MongoDB?
+        // Do we need to implement something?
+        return null;
     }
 
+    /**
+     * Get the Tuple based on an EntityKey.
+     * @param key The EntityKey.
+     * @return The Tuple based off the key.
+     */
     @Override
     public Tuple getTuple(EntityKey key) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        DBCollection dbCollection = provider.getDatabase().getCollection(key.getTable());
+        DBObject dbObject = dbCollection.findOne(key.getId());
+        if (dbObject != null) {
+            TupleSnapshot tupleSnapshot = new MapBasedTupleSnapshot(dbObject.toMap());
+            return new Tuple(tupleSnapshot);
+        }
+        return null;
     }
 
+    /**
+     * Get the database object based on a key.
+     * @param key The database row and table to get from.
+     * @return The database object.
+     */
+    private DBObject getDBObject(EntityKey key) {
+        DBCollection dbCollection = provider.getDatabase().getCollection(key.getTable());
+        DBObject dbObject = dbCollection.findOne(key.getId());
+        return dbObject;
+    }
+
+    /**
+     * Create a new collection and add a new document.
+     * @param key The key that represents the document.
+     * @return The Tuple that represents the new document.
+     */
     @Override
     public Tuple createTuple(EntityKey key) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        DBObject dbObject = new BasicDBObject();
+        if (key.getId() != null)
+            dbObject.put("id", key.getId());
+        provider.getDatabase().createCollection(key.getTable(), dbObject);
+        return getTuple(key);
     }
 
+    /**
+     * Update a document with a new Tuple value.
+     * @param tuple The Tuple to update to.
+     * @param key The key that represents the document to update to.
+     */
     @Override
     public void updateTuple(Tuple tuple, EntityKey key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        BasicDBObject basicDBObject = new BasicDBObject();
+        for (String tKey : tuple.getColumnNames()) {
+            basicDBObject.put(tKey, tuple.get(tKey));
+        }
+        provider.getDatabase().getCollection(key.getTable()).update(getDBObject(key), basicDBObject);
     }
 
+    /**
+     * Remove a document from a collection or drop a collection.
+     * @param key The key that represents the collection and or document.
+     */
     @Override
     public void removeTuple(EntityKey key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (key.getId() != null)
+            provider.getDatabase().getCollection(key.getTable()).remove(getDBObject(key));
+        else
+            provider.getDatabase().getCollection(key.getTable()).drop();
     }
 
     @Override
