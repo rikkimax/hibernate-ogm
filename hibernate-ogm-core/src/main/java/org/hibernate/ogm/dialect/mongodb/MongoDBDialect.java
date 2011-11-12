c4fbd4bfb379a3865b9dd0f892ac36f06fc21f78
rikki molecatert <pbdadmin@gmail.com>
2011-11-12 14:07:36 +1300

    OGM Wrong package originally.

--- hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/MongoDBDialect.java
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/MongoDBDialect.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.ogm.dialect.mongodb.impl;
+package org.hibernate.ogm.dialect.mongodb;
 
 import com.mongodb.*;
 import org.hibernate.LockMode;
