bd668509170649ea117db6b1b6794805e1d55fbb
rikki molecatert <pbdadmin@gmail.com>
2011-11-13 13:56:46 +1300

    OGM started work on MongoDB's dialect.

--- hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/MongoDBDialect.java
+++ hibernate-ogm-core/src/main/java/org/hibernate/ogm/dialect/mongodb/MongoDBDialect.java
@@ -20,22 +20,26 @@
  */
 package org.hibernate.ogm.dialect.mongodb;
 
-import com.mongodb.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import org.hibernate.LockMode;
 import org.hibernate.dialect.lock.LockingStrategy;
 import org.hibernate.id.IntegralDataTypeHolder;
 import org.hibernate.ogm.datastore.impl.EmptyTupleSnapshot;
 import org.hibernate.ogm.datastore.impl.MapBasedTupleSnapshot;
 import org.hibernate.ogm.datastore.mongodb.impl.MongoDBDatastoreProvider;
-import org.hibernate.ogm.datastore.spi.Association;
-import org.hibernate.ogm.datastore.spi.Tuple;
-import org.hibernate.ogm.datastore.spi.TupleSnapshot;
+import org.hibernate.ogm.datastore.spi.*;
 import org.hibernate.ogm.dialect.GridDialect;
-import org.hibernate.ogm.dialect.infinispan.InfinispanTupleSnapshot;
+import org.hibernate.ogm.dialect.infinispan.InfinispanAssociationSnapshot;
 import org.hibernate.ogm.grid.AssociationKey;
 import org.hibernate.ogm.grid.EntityKey;
 import org.hibernate.ogm.grid.RowKey;
 import org.hibernate.persister.entity.Lockable;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Dialect abstracting Hibernate OGM from the grid implementation
@@ -50,6 +54,7 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Constructor for the dialect.
+     *
      * @param provider The MongoDB provider.
      */
     MongoDBDialect(MongoDBDatastoreProvider provider) {
@@ -64,6 +69,7 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Get the Tuple based on an EntityKey.
+     *
      * @param key The EntityKey.
      * @return The Tuple based off the key.
      */
@@ -79,6 +85,7 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Get the database object based on a key.
+     *
      * @param key The database row and table to get from.
      * @return The database object.
      */
@@ -89,6 +96,7 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Create a new collection and add a new document.
+     *
      * @param key The key that represents the document.
      * @return The Tuple that represents the new document.
      */
@@ -102,8 +110,9 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Update a document with a new Tuple value.
+     *
      * @param tuple The Tuple to update to.
-     * @param key The key that represents the document to update to.
+     * @param key   The key that represents the document to update to.
      */
     public void updateTuple(Tuple tuple, EntityKey key) {
         BasicDBObject basicDBObject = new BasicDBObject();
@@ -115,6 +124,7 @@ public class MongoDBDialect implements GridDialect {
 
     /**
      * Remove a document from a collection or drop a collection.
+     *
      * @param key The key that represents the collection and or document.
      */
     public void removeTuple(EntityKey key) {
@@ -125,26 +135,84 @@ public class MongoDBDialect implements GridDialect {
     }
 
     public Association getAssociation(AssociationKey key) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return createAssociation(key);
     }
 
     public Association createAssociation(AssociationKey key) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        Map<RowKey, Map<String, Object>> ret = new HashMap<RowKey, Map<String, Object>>();
+        DBCollection col = provider.getDatabase().getCollection(key.getTable());
+        // Will assume to use all columns from db.
+        for (DBObject object : col.find().toArray()) {
+            Map map = new HashMap<String, Object>();
+            for (String sKey : object.keySet()) {
+                map.put(sKey, object.get(sKey));
+            }
+            RowKey rowKey = new RowKey(key.getTable(), (String[]) object.keySet().toArray(), map.values().toArray());
+            ret.put(rowKey, map);
+        }
+        return new Association(new InfinispanAssociationSnapshot(ret));
     }
 
     public void updateAssociation(Association association, AssociationKey key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        Map<RowKey, Map<String, Object>> atomicMap = ((InfinispanAssociationSnapshot) association.getSnapshot()).getAtomicMap();
+        for (AssociationOperation action : association.getOperations()) {
+            switch (action.getType()) {
+                case CLEAR:
+                    atomicMap.clear();
+                case PUT_NULL:
+                case PUT:
+                    atomicMap.put(action.getKey(), tupleToMap(action.getValue()));
+                    break;
+                case REMOVE:
+                    atomicMap.remove(action.getKey());
+                    break;
+            }
+        }
+        // Save everything...
     }
 
     public void removeAssociation(AssociationKey key) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        // Do nothing...
+        // Its just a variable that can be used to store stuff..
     }
 
     public Tuple createTupleAssociation(AssociationKey associationKey, RowKey rowKey) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return new Tuple(EmptyTupleSnapshot.SINGLETON);
     }
 
     public void nextValue(RowKey key, IntegralDataTypeHolder value, int increment, int initialValue) {
         //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    Map<String, Object> tupleToMap(Tuple tuple) {
+        if (tuple == null) {
+            return null;
+        }
+        Map<String, Object> snapshot;
+        TupleSnapshot snapshotInstance = tuple.getSnapshot();
+        if (snapshotInstance == EmptyTupleSnapshot.SINGLETON) {
+            //new assoc tuples are made of EmptyTupleSnapshot
+            snapshot = Collections.EMPTY_MAP;
+        } else {
+            //loaded assoc tuples are made of MapBasedTupleSnapshot
+            snapshot = ((MapBasedTupleSnapshot) snapshotInstance).getMap();
+        }
+        Map<String, Object> map = new HashMap<String, Object>(snapshot);
+        applyTupleOpsOnMap(tuple, map);
+        return map;
+    }
+
+    private void applyTupleOpsOnMap(Tuple tuple, Map<String, Object> map) {
+        for (TupleOperation action : tuple.getOperations()) {
+            switch (action.getType()) {
+                case PUT_NULL:
+                case PUT:
+                    map.put(action.getColumn(), action.getValue());
+                    break;
+                case REMOVE:
+                    map.remove(action.getColumn());
+                    break;
+            }
+        }
     }
 }
